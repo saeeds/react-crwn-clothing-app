4b02b81eeee5e47851c71fcd2cafb6551be36fa9
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-28 01:22:59 +0300

    Updates

---
+++ precache-manifest.c776453b651b7a452d28f6742d94bc3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4ba9d054d8c0ca6bdcc2e1def8f99f",
+    "url": "/react-crwn-clothing-app/index.html"
+  },
+  {
+    "revision": "184c939514394322117f",
+    "url": "/react-crwn-clothing-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ad20129af86abf1b1fec",
+    "url": "/react-crwn-clothing-app/static/js/2.b3eb80a0.chunk.js"
+  },
+  {
+    "revision": "184c939514394322117f",
+    "url": "/react-crwn-clothing-app/static/js/main.bfd65c55.chunk.js"
+  },
+  {
+    "revision": "edcf5304912c891bdd6c",
+    "url": "/react-crwn-clothing-app/static/js/runtime-main.f059118b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-crwn-clothing-app/static/media/logo.25bf045c.svg"
+  }
+]);
