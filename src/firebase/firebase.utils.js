b98a21385212fb1b6ce6bd9df1d8ec46649ece5a
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-10-06 16:10:45 +0300

    set basename for browser router to allow app working on github pages

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,7 +17,6 @@ export const createUserProfileDocument = async (userAuth, addtionalData) => {
   if (!userAuth) return;
   const userRef =  firestore.doc(`users/${userAuth.uid}`);
  
-  
   const snapShot = await userRef.get();
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
@@ -31,7 +30,6 @@ export const createUserProfileDocument = async (userAuth, addtionalData) => {
       })
     } catch (error) {
         console.log('error creating user', error.message);
-        
     } 
   }
   return userRef;
