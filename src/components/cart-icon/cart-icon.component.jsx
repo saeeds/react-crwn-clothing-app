4983a5e2f2592057926f69ce29c222ab49c0ae1d
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-10-27 21:46:08 +0300

    updating our application with reselect, replaced cart selectors with memoized selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-coun">0</span>
+      <span className="item-coun">{itemCount}</span>
     </div>
   );
 };
@@ -18,7 +18,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./collection-preview.styles.scss";
-import CollectionItem from '../collection-item/collection-item.components'
-const CollectionPreview = ({ title,items }) => {
+import CollectionItem from "../collection-item/collection-item.components";
+const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-      { 
-          items
+        {items
           .filter((item, idx) => idx < 4)
-          .map(({id, ...otherItemProps}) => (
-              <CollectionItem key={id} {...otherItemProps} />
-          ))
-      }
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
       </div>
     </div>
   );
