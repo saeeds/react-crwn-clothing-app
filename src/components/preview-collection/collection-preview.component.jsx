e47c3122294cb925442df8e942b2f373c24aa931
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-10-28 20:08:31 +0300

    moved our directory and shop data into its own reducer. created respective selectors and updated corresponding components with new redux

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.components";
 const CollectionPreview = ({ title, items }) => {
+  console.log(title, items);
+  
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
